3ce405be33003b60d744d6530043ea33aa4e68b3
Arunasjok <jokubynas@gmail.com>
2022-10-31 16:36:56 +0000

    DEAD END with donate menu....

--- app/src/main/java/ie/wit/ufopedia/fragments/UfoListFragment.kt
+++ app/src/main/java/ie/wit/ufopedia/fragments/UfoListFragment.kt
@@ -1,23 +1,42 @@
 package ie.wit.ufopedia.fragments
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import ie.wit.ufopedia.R
+import ie.wit.ufopedia.activities.UfoActivity
+import ie.wit.ufopedia.adapters.UfoAdapter
+import ie.wit.ufopedia.databinding.FragmentUfoListBinding
+import ie.wit.ufopedia.main.MainApp
+import ie.wit.ufopedia.models.UfoModel
+
 
 class UfoListFragment : Fragment() {
 
+    lateinit var app: MainApp
+    private var _fragBinding: FragmentUfoListBinding? = null
+    private val fragBinding get() = _fragBinding!!
+    private lateinit var refreshIntentLauncher : ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = activity?.application as MainApp
+        setHasOptionsMenu(true)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_donate, container, false)
+        _fragBinding = FragmentUfoListBinding.inflate(inflater, container, false)
+        val root = fragBinding.root
+        activity?.title = getString(R.string.action_ufoList)
+
+        return root;
     }
 
     companion object {
@@ -27,4 +46,6 @@ class UfoListFragment : Fragment() {
                 arguments = Bundle().apply {}
             }
     }
+
 }
+
